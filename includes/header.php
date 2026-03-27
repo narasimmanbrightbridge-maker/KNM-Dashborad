9ecf685c9b4838f6b5107f66e1e4c53a1b8ef982
narasimman <narasimmanbrightbridge@gmail.com>
2026-03-27 09:47:16 +0530

    branch-onr

---
+++ includes/header.php
@@ -0,0 +1,83 @@
+    <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+        <a class="navbar-brand brand-logo" href="#"><img src="assets/images/logo.svg" alt="logo" /></a>
+        <a class="navbar-brand brand-logo-mini" href="#"><img src="assets/images/logo.svg" alt="logo" /></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-stretch">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="mdi mdi-menu"></span>
+        </button>
+        <div class="search-field d-none d-md-block">
+          <form class="d-flex align-items-center h-100" action="#">
+            <div class="input-group">
+              <div class="input-group-prepend bg-transparent">
+                <i class="input-group-text border-0 mdi mdi-magnify"></i>
+              </div>
+              <input type="text" class="form-control bg-transparent border-0" placeholder="Search projects">
+            </div>
+          </form>
+        </div>
+        <ul class="navbar-nav navbar-nav-right">
+
+
+
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="mdi mdi-menu"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span class="menu-title">Dashboard</span>
+              <i class="mdi mdi-home menu-icon"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <span class="menu-title">Schools</span>
+              <i class="menu-arrow"></i>
+              <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link" href="">Buttons</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="">Dropdowns</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="">Typography</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span class="menu-title">Costings</span>
+              <i class="mdi mdi-home menu-icon"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span class="menu-title">Invoice</span>
+              <i class="mdi mdi-home menu-icon"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span class="menu-title">Users</span>
+              <i class="mdi mdi-home menu-icon"></i>
+            </a>
+          </li>
+
+
+        </ul>
+      </nav>
---
+++ login.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Health Basix | KNM Dashboard</title>
+  <?php include './includes/css.php'; ?>
+</head>
+
+<body>
+
+<section class="loginsec">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 pl0">
+                <div class="login_img">
+                    <img src="assets/images/login-img-one.webp" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 bg-background">
+                <div class="loginForm">
+                    <img class="mb-4" src="assets/images/logo-two.svg" alt="">
+                    <h2 class="mb-4">Login</h2>
+                    <form action="">
+                        
+                        <div class="col-lg-12 mb-3">
+                            <input type="text" name="username" placeholder="username">
+                        </div>
+
+                        <div class="col-lg-12">
+                            <input type="text" name="password" placeholder="Password">
+                        </div>
+
+                        <p class="pt-3">Forgetpassword</p>
+
+                        <button>Login</button>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+</body>
+
+</html>
